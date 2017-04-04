2dbe2421fb2c4a28936aedddda8665861a5f88cf
ZeroStack <sahilmp8@gmail.com>
2017-04-04 21:38:57 +1000

    ADD: screen

--- R/screen.R
+++ R/screen.R
@@ -1,1 +1,20 @@
+#' Screen for stocks
+#'
+#'@importFrom stringr str_c
+#'
+#'@export
+screen <- function(exchange) {
+  str_c(
+    #open square bracket
+    "%5B",
+    exchange
+
+
+
+    #closing square bracket
+    "%5D"
+
+  )
+
+}
 
