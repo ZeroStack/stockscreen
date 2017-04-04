a14ebf299104fa27c0755373c5b735c07b32c5e7
ZeroStack <sahilmp8@gmail.com>
2017-04-04 21:51:02 +1000

    ADD

--- R/screen.R
+++ R/screen.R
@@ -1,10 +1,13 @@
 #' Screen for stocks
 #'
 #'@importFrom stringr str_c
+#'@importFrom httr GET
+#'@importFrom jsonlite fromJSON
 #'
 #'@export
 screen <- function(exchange) {
-  temp.req <- str_c(get('url_key', envir = .url_key),
+  # Build the json request
+  temp.url <- str_c(get('url_key', envir = .url_key),
     #open square bracket
     "%5B",
     exchange,
@@ -15,6 +18,12 @@ screen <- function(exchange) {
 
   )
 
+  # request the data
+  url.data <- try(httr::GET(temp.url, add_headers("user-agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36", "Cache-Control" = "no-cache")))
+
+  url.extract <- gsub("\\", "", url.data, fixed = TRUE)
+
+  url.parse <- jsonlite::fromJSON(url.extract, flatten=TRUE)
 }
 
 
