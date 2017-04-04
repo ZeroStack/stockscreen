dc5db7fd729886f85c86ed3891088a847e210f3c
ZeroStack <sahilmp8@gmail.com>
2017-04-04 22:08:02 +1000

    ADD

--- R/screen.R
+++ R/screen.R
@@ -1,22 +1,23 @@
 #' Screen for stocks
 #'
 #'@importFrom stringr str_c
-#'@importFrom httr GET
+#'@importFrom httr GET add_headers
 #'@importFrom jsonlite fromJSON
 #'
 #'@export
 screen <- function(exchange) {
   # Build the json request
-  temp.url <- str_c(get('url_key', envir = .url_key),
+  temp.url <<- str_c(get('url_key', envir = .url_key),
     #open square bracket
-    "%5B",
-    exchange,
+    #"%5B",
+    exchange
 
 
     #closing square bracket
-    "%5D"
+    #"%5D"
 
   )
+
 
   # request the data
   url.data <- try(httr::GET(temp.url, add_headers("user-agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36", "Cache-Control" = "no-cache")))
