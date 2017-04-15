f2468425cd20c28d6ead405e8e83bd274fb3b53a
ZeroStack <sahilmp8@gmail.com>
2017-04-15 22:08:18 +1000

    Add further functionality

--- R/screen.R
+++ R/screen.R
@@ -5,20 +5,44 @@
 #'@importFrom jsonlite fromJSON
 #'
 #'@export
-screen <- function(market_cap = list()) {
+screen <- function(market_cap = list(), pe_ratio = list(),
+                   dividend_yield = list(), price_change_52week = list(),
+                   price_to_book = list(),
+                   current_assets_to_liabilities_ratio_year = list(),
+                   longterm_debt_to_equity_year = list(),
+                   return_on_equity_5years = list(),
+                   net_income_growth_rate_5years = list(),
+                   revenue_growth_rate_10years = list(),
+                   eps_growth_rate_10years = list(),
+                   interest_coverage_year = list()
+                   ) {
   
   #Lookup for temp.url
   lookup <- c('[' = '%5B', ']' = '%5D', '(' = '%28', ')' = '%29', '>' = '>', '<' = '<', '=' = '%3D', '&' = '%26')
   
+  print(lookup)
   # Build the json request
-  temp.url <- str_c(get('url_key', envir = .url_key),
+  temp.url <<- str_c(get('url_key', envir = .url_key),
     #open square bracket
     lookup[['[']],
-    
-
-
+    stringr::str_c(formulate_criteria('market_cap', market_cap, lookup),
+                   formulate_criteria('pe_ratio', pe_ratio, lookup),
+                   formulate_criteria('dividend_yield', dividend_yield, lookup),
+                   formulate_criteria('price_change_52week', price_change_52week, lookup),
+                   formulate_criteria('price_to_book', price_to_book, lookup),
+                   formulate_criteria('current_assets_to_liabilities_ratio_year', current_assets_to_liabilities_ratio_year, lookup),
+                   formulate_criteria('longterm_debt_to_equity_year', longterm_debt_to_equity_year, lookup),
+                   formulate_criteria('return_on_equity_5years', return_on_equity_5years, lookup),
+                   formulate_criteria('net_income_growth_rate_5years', net_income_growth_rate_5years, lookup),
+                   formulate_criteria('revenue_growth_rate_10years', revenue_growth_rate_10years, lookup),
+                   formulate_criteria('eps_growth_rate_10years', eps_growth_rate_10years, lookup),
+                   formulate_criteria('interest_coverage_year', interest_coverage_year, lookup),
+                   sep = str_c('+', lookup[['&']], '+')
+                   ),
     #closing square bracket
-    lookup[[']']]
+    lookup[[']']],
+    #'&restype=company&output=json&noIL=1&ei=-RPuWKGMMIuc0AT0tL6IBQ'
+    '&output=json&ei=57VwWOnoM4mi0wTw_oagBA'
 
   )
 
@@ -32,21 +56,45 @@ screen <- function(market_cap = list()) {
 }
 
 
-formulate_criteria <- function(criteria_name, criteria) {
-  # initalise counter
-  i <- 1
+formulate_criteria <- function(criteria_name, criteria, lookup) {
+  
+  options(scipen = 999)
+  # temporary environment
+  formulateEnv <- new.env()
+  
+  # Assign switch
+  assign(x = 'i',
+         value = 1,
+         envir = formulateEnv
+  )
+   
+  
   temp <- purrr::map(criteria, function(x) {
+    
+    # Get i
+    i <- get(x = 'i',
+             envir= formulateEnv
+    )
+    print(i)
+    
+  
     if(i == 1) {
-      operator <- lookup[['<']]
+      print('here')
+      operator <- lookup[['>']]
+      
+      assign(x = 'i',
+             value = 2,
+             envir = formulateEnv
+      )
     } else {
+      print('here 2')
       operator <- lookup[['<']]
     }
-    
-    #increment counter
-    i <- i + 1
+    print(x)
     stringr::str_c(lookup[['(']], criteria_name, '+', operator , lookup[['=']], '+',  x, lookup[[')']] )
   })
   
+  rm(formulateEnv)
   temp <- str_c(temp, collapse = stringr::str_c('+', lookup[['&']], '+') )
 }
 
